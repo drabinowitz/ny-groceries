f1c5571ea175bbc985abd5757acae879a29891f9
dmitri <dmitri@vistarmedia.com>
2015-11-07 09:22:22 -0500

    Switch api to sqlite

--- api/wiki.go
+++ api/wiki.go
@@ -4,9 +4,10 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
+	"time"
 )
 
 type Store struct {
@@ -39,10 +40,6 @@ type Product struct {
 type ReceiptUpload struct {
 	Receipt   Receipt    `json:"receipt"`
 	Purchases []Purchase `json:"purchases"`
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>STUFF</h1><div>STUFF</div>")
 }
 
 var AllStores []Store = make([]Store, 0)
@@ -115,7 +112,7 @@ func productsHandler(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 			}
-			statement := "INSERT INTO products (%s) VALUES (%s) RETURNING id"
+			statement := "INSERT INTO products (%s) VALUES (%s)"
 			if product.Sub_category != "" {
 				data := fmt.Sprintf("'%s', '%s'", product.Category, product.Sub_category)
 				statement = fmt.Sprintf(statement, "category, sub_category", data)
@@ -124,9 +121,17 @@ func productsHandler(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
 				statement = fmt.Sprintf(statement, "category", data)
 			}
 			var productId int64
-			err = db.QueryRow(statement).Scan(&productId)
+			_, err = db.Exec(statement)
+			productIds, err := db.Query("SELECT last_insert_rowid() FROM products")
 			if err != nil {
 				log.Fatal(err)
+			}
+			defer productIds.Close()
+			for productIds.Next() {
+				err := productIds.Scan(&productId)
+				if err != nil {
+					log.Fatal(err)
+				}
 			}
 			product.Id = productId
 			AllProducts = append(AllProducts, product)
@@ -183,24 +188,36 @@ func receiptUploadsHandler(db *sql.DB) func(w http.ResponseWriter, r *http.Reque
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 			}
-			if len(receiptUpload.Purchases) == 0 {
-				http.Error(w, "No purchases with receipt", http.StatusInternalServerError)
-			}
 			receipt := receiptUpload.Receipt
-			receiptStatement := fmt.Sprintf(`
-				INSERT INTO receipts (store_id, total, date)
-				VALUES (%v, %v, '%s') RETURNING id
-			`, receipt.Store_id, receipt.Total, receipt.Date)
-			var receiptId int64
-			err = db.QueryRow(receiptStatement).Scan(&receiptId)
+			receiptDate, err := time.Parse("01/02/2006", receipt.Date)
 			if err != nil {
 				log.Fatal(err)
+			}
+			receiptStatement := fmt.Sprintf(`
+				INSERT INTO receipts (store_id, total, date)
+				VALUES (%v, %v, '%s')
+			`, receipt.Store_id, receipt.Total, receiptDate.Format("2006-01-02"))
+			var receiptId int64
+			_, err = db.Exec(receiptStatement)
+			if err != nil {
+				log.Fatal(err)
+			}
+			receiptIds, err := db.Query("SELECT last_insert_rowid() FROM receipts")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer receiptIds.Close()
+			for receiptIds.Next() {
+				err := receiptIds.Scan(&receiptId)
+				if err != nil {
+					log.Fatal(err)
+				}
 			}
 			receiptUpload.Receipt.Id = receiptId
 			purchases := receiptUpload.Purchases
 			purchaseString := `
 				INSERT INTO purchases (receipt_id, quantity, cost, product_id, unit)
-				VALUES (%v, %v, %v, %v, '%s') RETURNING id
+				VALUES (%v, %v, %v, %v, '%s')
 			`
 			for i := range purchases {
 				p := &receiptUpload.Purchases[i]
@@ -213,9 +230,20 @@ func receiptUploadsHandler(db *sql.DB) func(w http.ResponseWriter, r *http.Reque
 				purchaseStatement := fmt.Sprintf(purchaseString,
 					receiptId, p.Quantity, p.Cost, p.Product_id, p.Unit)
 				var purchaseId int64
-				err = db.QueryRow(purchaseStatement).Scan(&purchaseId)
+				_, err = db.Exec(purchaseStatement)
 				if err != nil {
 					log.Fatal(err)
+				}
+				purchaseIds, err := db.Query("SELECT last_insert_rowid() FROM purchases")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer purchaseIds.Close()
+				for purchaseIds.Next() {
+					err := purchaseIds.Scan(&purchaseId)
+					if err != nil {
+						log.Fatal(err)
+					}
 				}
 				p.Id = purchaseId
 				p.Receipt_id = receiptId
@@ -228,7 +256,7 @@ func receiptUploadsHandler(db *sql.DB) func(w http.ResponseWriter, r *http.Reque
 }
 
 func main() {
-	db, err := sql.Open("postgres", "postgres:///groceries")
+	db, err := sql.Open("sqlite3", "./apidb.db")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -237,7 +265,6 @@ func main() {
 	loadStores(db)
 	loadProducts(db)
 
-	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/stores/", storesHandler)
 	http.HandleFunc("/products/", productsHandler(db))
 	http.HandleFunc("/receipt_uploads/", receiptUploadsHandler(db))
