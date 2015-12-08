38de4f7d54351305bdb75ef8faaab490543f01f7
dmitri <dmitri@vistarmedia.com>
2015-12-07 21:43:18 -0500

    Finish requested products table

--- query-ui/app/component/requested_products_table.js
+++ query-ui/app/component/requested_products_table.js
@@ -1,21 +1,53 @@
 import React from 'react';
+import ObjectTable from 'cmpnt/table/object-table';
 
 
 class RequestedProductsTable extends React.Component {
+  filter(term, row) {
+    return row.name.indexOf(term) > -1
+  }
+
   render() {
     let products = this.props.products
       .filter(product => this.props.requestedProducts[product.id])
-      .sort((a, b) => {
-        if (a.category != b.category) {
-          return a.category > b.category;
-        } else {
-          return a.sub_category > b.sub_category;
-        }})
       .map(product => {
         let name =
           `#{product.category}/#{product.sub_category?product.sub_category:""}`;
         let productCosts = this.props.requestedProducts[product.id];
+        return {
+          name,
+          ...productCosts.stores,
+        };
       });
+
+    return(
+      <ObjectTable
+        filter = {this.filter}
+        rows = {products}
+        columns = {this.columns()}
+      />
+    );
+  }
+
+  columns() {
+    firstField = {field: 'name', label: 'Name'};
+    storeFields = this.props.stores.map(store => {
+      return {
+        field: `#{store.id}`,
+        label: store.name,
+        format: storeCosts => {
+          if (!storeCosts) {
+            return '--';
+          }
+          let result = [];
+          for (let unit in storeCosts) {
+            let {quantity, cost} = storeCosts[unit];
+            result.push(`#{(cost/quantity).toFixed(2)} #{unit}`)
+          }
+          return result.join(', ');
+        }
+      };
+    });
   }
 }
 
